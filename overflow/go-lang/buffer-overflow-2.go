a2a9c87ecf803c97b706e9e8341596fe0a9d4bc9
kajaaz <karolinagorna@hotmail.fr>
2023-11-07 16:32:33 +0000

    update

---
+++ overflow/go-lang/buffer-overflow-2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+        "fmt"
+        "unsafe"
+)
+
+func vulnerableFunction(userInput []byte) {
+        var buffer [10]byte
+        for i := 0; i < len(userInput); i++ {
+                buffer[i] = userInput[i]  // This line causes a buffer overflow when userInput is longer than 10 bytes
+        }
+        fmt.Println(string(buffer[:]))
+}
+
+// Here is the new function that prints "Hello, World!"
+func helloWorld() {
+        fmt.Println("Hello, World!")
+}
+
+func main() {
+        var input string
+        fmt.Println("Enter input:")
+        fmt.Scan(&input)
+        vulnerableFunction([]byte(input))
+        // This line demonstrates the issue by printing the address of buffer
+        fmt.Printf("Address of buffer: %p\n", unsafe.Pointer(&input))
+        
+        // Call the helloWorld function
+        helloWorld()
+}
